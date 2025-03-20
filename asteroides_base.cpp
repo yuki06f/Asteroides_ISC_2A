a2570e63a7d2417ad992aa0bb485189c120b2f97
yuki06f <ferhdz.06f@outlook.com>
2025-03-19 19:27:17 -0600

    Create asteroides_base.cpp
    Base inicial del código de asteroides, cada quien trabaja en su parte de forma aislada y cuando estén listas las partes individuales las conectamos aquí para empezar a pegar todo :)

---
+++ asteroides_base.cpp
@@ -0,0 +1,34 @@
+/* PROYECTO FINAL PROGRAMACIÓN 1: ASTEROIDES
+INTEGRANTES DEL EQUIPO: jOSHUA IVÁN , DIEGO JOAO SÁNCHEZ LÓPEZ, RUBÉN ALEJANDRO HERNÁNDEZ DE LA TORRE Y MARIA FERNANDA HERNANDEZ PÉREZ PEÑA.
+GRADO Y GRUPO: ISC-2A
+*/
+
+#include <stdio.h> //La librería stdio.h sirve para funciones básicas para mostrar y leer datos, asi como para el manejo de archivos
+#include <conio.h> //La libreria conio.h la usamos para poder hacr uso de metodos de lectura especificos (como getch)
+#include <stdlib.h> //La libreria stdlib.h nos sirve para aplicar la memoria dinamica y usar numeros aleatorios
+#include <string.h> //la libreria string.h nos sirve para manejar de mejor manera los strings y modificarlos
+#include <time.h> //la libreria time.h se usa para el manejo de numeros aleatorios
+#include <windows.h> //la libreria windows.h nos sirve para manejar la pantalla para usar coordenadas
+
+#incldue <graphics.h> //para el uso de graficos
+
+//PROTOTIPOS DE LAS FUNCIONES
+
+//FUNCIONES SIN PARAMETROS Y SIN RETORNO
+
+
+//FUNCIONES CON PARAMETROS Y SIN RETORNO
+
+
+//FUNCIONES SIN PARAMETROS Y CON RETORNO
+
+
+
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
+
+//IMPLEMENTACION DE LAS FUNCIONES
