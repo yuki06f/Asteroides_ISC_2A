c9dd629fec74f8dbd1907c5a1d2303ad42718229
yuki06f <ferhdz.06f@outlook.com>
2025-03-19 19:29:02 -0600

    Update asteroides_base.cpp

--- asteroides_base.cpp
+++ asteroides_base.cpp
@@ -1,5 +1,5 @@
 /* PROYECTO FINAL PROGRAMACIÓN 1: ASTEROIDES
-INTEGRANTES DEL EQUIPO: jOSHUA IVÁN , DIEGO JOAO SÁNCHEZ LÓPEZ, RUBÉN ALEJANDRO HERNÁNDEZ DE LA TORRE Y MARIA FERNANDA HERNANDEZ PÉREZ PEÑA.
+INTEGRANTES DEL EQUIPO: jOSHUA IVÁN BRAVO GONZÁLEZ, DIEGO JOAO SÁNCHEZ LÓPEZ, RUBÉN ALEJANDRO HERNÁNDEZ DE LA TORRE Y MARIA FERNANDA HERNANDEZ PÉREZ PEÑA.
 GRADO Y GRUPO: ISC-2A
 */
 
@@ -10,7 +10,7 @@ GRADO Y GRUPO: ISC-2A
 #include <time.h> //la libreria time.h se usa para el manejo de numeros aleatorios
 #include <windows.h> //la libreria windows.h nos sirve para manejar la pantalla para usar coordenadas
 
-#incldue <graphics.h> //para el uso de graficos
+#include <graphics.h> //para el uso de graficos
 
 //PROTOTIPOS DE LAS FUNCIONES
 
